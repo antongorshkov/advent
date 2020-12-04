2522c7e14b820f26903723b24b532abce43573e3
Anton Gorshkov <antong@gmail.com>
2020-12-04 17:21:53 -0500

    ok, I think I'm happy now. Short, but still readable.

--- day4.py
+++ day4.py
@@ -8,19 +8,16 @@ def validateEcl(x): return x in {'amb','blu','brn','gry','grn','hzl','oth'}
 def validatePid(x): return x.isdigit() and len(x) == 9
 def validateHcl(x): return re.match(r"#[0-9a-f]{6}", x) is not None
 def validateHgt(x): return (("cm" in x and (150 <= int(x.replace("cm", "")) <= 193)) or ("in" in x and (59 <= int(x.replace("in", "")) <= 76)))
+def isAllFields(x): return len(requiredFields.difference(x)) == 0
 
 lines = []
 passportsRaw = open("day4.input", "r").read().split("\n\n")
 
 isValidCnt = 0
 for passport in passportsRaw:
-    fields = passport.split()
-    p = {}
-    for field in fields:
-        [k,v] = field.split(':')
-        p[k]=v
-    p.pop('cid', None)
-    if (len(requiredFields.difference(p.keys())) == 0
+    p = {f.split(':')[0]:f.split(':')[1] for f in passport.split()}
+    isValidCnt += 1 if (
+            isAllFields(p.keys())
         and validateByr(p['byr'])
         and validateIyr(p['iyr'])
         and validateEyr(p['eyr'])
@@ -28,7 +25,7 @@ for passport in passportsRaw:
         and validateHcl(p['hcl'])
         and validateEcl(p['ecl'])
         and validatePid(p['pid'])
-    ):
-        isValidCnt += 1
+    ) else 0
+
 
 print isValidCnt

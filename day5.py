14ac3de71d387b78955facbe9fca2bb1b48bffc7
Anton Gorshkov <antong@gmail.com>
2020-12-05 11:21:33 -0500

    it's all one big binary number, no need for rows or cols!

--- day5.py
+++ day5.py
@@ -1,12 +1,9 @@
 from string import maketrans
 
-def getSid(row,col): return row * 8 + col
 def decodePass(input):
     table = maketrans('FBLR', '0101')
     input = input.translate(table)
-    row = int(input[0:7],2)
-    col = int(input[7:10],2)
-    return [row,col]
+    return int(input,2)
 
 def find_missing(lst):
     return sorted(set(range(lst[0], lst[-1] + 1)).difference(lst))
@@ -14,8 +11,7 @@ def find_missing(lst):
 allSids = []
 with open('day5.input') as inputFile:
     for line in inputFile:
-        [row,col] = decodePass(line)
-        allSids.append(getSid(row,col))
+        allSids.append(decodePass(line))
 
 print max(allSids)
 print find_missing(allSids)

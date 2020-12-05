4b8d9c66ae487ac492c68298f89775d8b723ddb8
Anton Gorshkov <antong@gmail.com>
2020-12-05 11:03:26 -0500

    introduce maketrans for readability

--- day5.py
+++ day5.py
@@ -1,15 +1,15 @@
+from string import maketrans
+
 def getSid(row,col): return row * 8 + col
 def decodePass(input):
-    for letter, number in {'F':'0','B':'1','R':'1','L':'0'}.items():
-        input = input.replace(letter, number)
+    table = maketrans('FBLR', '0101')
+    input = input.translate(table)
     row = int(input[0:7],2)
     col = int(input[7:10],2)
     return [row,col]
 
 def find_missing(lst):
-    start = lst[0]
-    end = lst[-1]
-    return sorted(set(range(start, end + 1)).difference(lst))
+    return sorted(set(range(lst[0], lst[-1] + 1)).difference(lst))
 
 allSids = []
 with open('day5.input') as inputFile:

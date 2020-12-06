283cbfff8e778ff76f2e078e11dad025a702db19
Anton Gorshkov <antong@gmail.com>
2020-12-06 11:31:31 -0500

    ensure consistent quoting + move strip up to reading of file. ok, think I'm happy now

--- day6.py
+++ day6.py
@@ -1,4 +1,4 @@
-groupInputs = open("day6.input", "r").read().split("\n\n")
+groupInputs = open('day6.input', 'r').read().strip().split('\n\n')
 def pt1():
     total = 0
     for gInput in groupInputs:
@@ -9,7 +9,7 @@ def pt1():
 def pt2():
     total = 0
     for line in groupInputs:
-        lines = [set(line) for line in line.strip().split("\n")]
+        lines = [set(line) for line in line.split('\n')]
         total += len(reduce(lambda x, y: x & y, lines))
     return total
 print pt1()

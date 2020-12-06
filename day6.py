c51bbfdf37b7c86f7651122291b9894eda913f33
Anton Gorshkov <antong@gmail.com>
2020-12-06 11:26:06 -0500

    return values instead of printing them inside functions

--- day6.py
+++ day6.py
@@ -4,13 +4,13 @@ def pt1():
     for gInput in groupInputs:
         gInput = gInput.replace('\n','')
         total += len(set(gInput))
-    print total
+    return total
 
 def pt2():
     total = 0
     for line in groupInputs:
         lines = [set(line) for line in line.strip().split("\n")]
         total += len(reduce(lambda x, y: x & y, lines))
-    print total
-pt1()
-pt2()
+    return total
+print pt1()
+print pt2()

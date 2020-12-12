c7b7c4c54bf515fbec82630e6773a52d94c8f2cd
Anton Gorshkov <antong@gmail.com>
2020-12-11 20:34:33 -0500

    day 8 pt2 works now!

--- day8.py
+++ day8.py
@@ -1,28 +1,26 @@
 instructions = [x.split(' ') for x in open('day8.input', 'r').read().strip().split('\n')]
-# instructions = [x.split(' ') for x in open('day8.sample', 'r').read().strip().split('\n')]
 
 nodes = {}
 i = 0
 while i < len(instructions):
-    if instructions[i][0] == 'acc':
-        nodes[i + 1] = i
-    elif instructions[i][0] == 'jmp':
-        nodes[i + int( instructions[i][1] )] = i
-    else:
-        nodes[i + 1] = i
+    increment = int( instructions[i][1] ) if instructions[i][0] == 'jmp' else 1
+    nodes[i] = i + increment
     i += 1
 
-# print nodes
+def isTerminal(nodeIndex):
+    visited = set()
+    while True:
+        if nodeIndex in visited: return False
+        if nodeIndex == len(nodes) - 1: return True
+        visited.add(nodeIndex)
+        nodeIndex = nodes[nodeIndex]
 
 def pt1():
     visited = set()
     i = 0
     acc = 0
     while i < len(instructions):
-        #if i == 640: print 'visited 640!!!'
         if i in visited:
-            print visited
-            print len(visited)
             return acc
         visited.add(i)
         if instructions[i][0] == 'acc':
@@ -33,43 +31,34 @@ def pt1():
         i += 1
 
 def pt2():
-    visited = set()
-    terminationPath = set()
-    i = len(instructions) - 1
-    while i > 0:
-
-        if i in terminationPath: break
-        terminationPath.add(i)
-        if i not in nodes: break
-        i = nodes[i]
-        print terminationPath
-    visited = set()
+    tNodes = set(filter(isTerminal, nodes.keys()))
     i = 0
     acc = 0
+    visited = set()
+    switched = False
     while i < len(instructions):
-        if i in visited:
-            # print visited
-            # print len(visited)
-            return acc
+        if i in visited: return acc
         visited.add(i)
-        inst = instructions[i][0]
-        if inst == 'jmp' and (i+1) in terminationPath:
-            print 'jpm change at ', i
-            inst = 'nop'
-        elif inst == 'nop' and (i+int( instructions[i][1] )) in terminationPath:
-            print 'nop change at ', i
-            inst = 'jmp'
 
-        if inst == 'acc':
+        if not switched and instructions[i][0] == 'jmp' and (i + 1) in tNodes:
+            instructions[i][0] = 'nop'
+            switched = True
+        elif not switched and instructions[i][0] == 'nop' and (i + int( instructions[i][1] )) in tNodes:
+            print 'found nop'
+            instructions[i][0] = 'jmp'
+            switched = True
+
+        if instructions[i][0] == 'acc':
             acc += int( instructions[i][1] )
-            i += 1
-        elif inst == 'jmp':
+        elif instructions[i][0] == 'jmp':
             i += int( instructions[i][1] )
-        else:
-            i += 1
+            continue
+        i += 1
+
     return acc
 
 
 
 #print pt1()
 print pt2()
+# print instructions
